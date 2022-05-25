9a5e3a43e757f340889eb2eac6b417ad854d6bbc
carlosmsx <carlosmsx@gmail.com>
2022-05-25 19:50:19 -0300

    agregado ejercicio 10

---
+++ js/tp2-ejercicio-10.js
@@ -0,0 +1,21 @@
+let nCols = parseInt(prompt("Ingrese el número de columas (entre 1 y 12)"))
+let nRows = parseInt(prompt("Ingrese el número de filas"))
+
+//TODO: validar valores ingresados
+
+let cuenta = nRows * nCols;
+
+document.write('<table class="container table table-bordered">');
+document.write('<tbody>');
+for (let row=0; row<nRows; row++)
+{
+  document.write('<tr>');
+for (let col=0; col<nCols; col++)
+{
+  document.write('<td class="text-center">'+cuenta+'</td>');
+  cuenta--;
+}
+document.write('</tr>');
+}
+document.write('</tbody>');
+document.write('</table>');
